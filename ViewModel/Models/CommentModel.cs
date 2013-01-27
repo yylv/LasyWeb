8216554e42eaedb18e5956fa5a4c6bb98f91f5b2
cqychen <chen_yu_qin_g@163.com>
2013-01-27 18:47:44 +0800

    Add change

---
+++ ViewModel/Models/CommentModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViewModel.Models
+{
+    public class CommentModel
+    {
+        [Display(Name = "用户ID")]
+        public string UserID { get; set; }
+
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "评价内容")]
+        public string CommentContent { get; set; }
+
+        [Required]
+        [Display(Name = "评价帖子")]
+        public string CommentTopic { get; set; }
+
+        [Required]
+        [Display(Name = "评论时间")]
+        public DateTime CommentTime { get; set; }
+    }
+}
