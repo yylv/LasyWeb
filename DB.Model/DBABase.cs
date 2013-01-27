d9259c84dbe6be0e08debec6b832ec39fe4efe7f
Yulong.Li <lyylv2010@gmail.com>
2013-01-27 13:04:10 +0800

    Init
    Init

---
+++ DB.Model/DBABase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB.Model.Models;
+
+namespace DB.Model
+{
+    public class DBABase
+    {
+        object thisLock = new object();
+        private static LasyDBContext dBContext;
+        protected LasyDBContext DBContext 
+        {
+            get 
+            {
+                if (DBABase.dBContext == null)
+                {
+                    lock (thisLock)
+                    {
+                        if (DBABase.dBContext == null)
+                        {
+                            DBABase.dBContext = new LasyDBContext();
+                        }
+                    }
+                }
+                return DBABase.dBContext;
+            }
+        }
+    }
+}
